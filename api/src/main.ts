f84c3c201e33d676b17587d91984d923775aa10d
sxrtori <sartorikk2@gmail.com>
2026-04-18 23:58:05 -0300

    feat: prepara backend para deploy

--- api/src/main.ts
+++ api/src/main.ts
@@ -32,10 +32,12 @@ async function bootstrap() {
     }),
   );
 
-  const port = Number(process.env.PORT || 3001);
+  const port = process.env.PORT || 3001;
+
   await app.listen(port, '0.0.0.0');
-  console.log(`API SportX iniciada na porta ${port}`);
+
+  console.log(`🚀 API SportX iniciada na porta ${port}`);
+  console.log('CWD =>', process.cwd());
 }
-console.log('CWD =>', process.cwd());
 
 void bootstrap();
